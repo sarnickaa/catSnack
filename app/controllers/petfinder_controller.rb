f9c8f2861b6d30e23bf5046f6d31514057933b96
sarnickaa <sarnicka.aleks@gmail.com>
2018-07-25 16:30:52 -0400

    added some code comments
    Dannys code for setting up 3rd party API's

--- app/controllers/petfinder_controller.rb
+++ app/controllers/petfinder_controller.rb
@@ -5,15 +5,26 @@ class PetfinderController < ApplicationController
   rescue StandardError => e
     render json: { errors: e.message }, status: :unprocessable_entity
   end
-# end
-
-# def index
-   # new Petfinder object
-  #   @petFinder = Petfinder.new
-  #   # now get shelters from @petFinder:
-  #   @shelters = @petFinder.shelters('02906')
-  #   render json: @shelters, status: :ok
-  # rescue StandardError => e
-  #   render json: { errors: e.message }, status: :unprocessable_entity
-  # end
 end
+
+# pet_finders_controller.rb
+
+
+# Dannys code:
+# class PetFindersController < ApplicationController
+#
+# # if you need just shelters, you should probably setup another controller action to just return shelters, so you have an #index method and a #shelters method on this controller
+# # this will require an additional route to access the shelters
+#
+#   def index
+#     @pet_finders = Petfinder.all # index action expects to return all of a resource
+#     # rest of index code
+#   end
+#
+#   def shelters
+#     @shelters = PetFinder.shelters
+#
+#     render json: @shelters, status: :ok
+#   rescue StandardError => e
+#     render json: { errors: e.message }, status: :unprocessable_entity
+#   end
--- app/models/petfinder.rb
+++ app/models/petfinder.rb
@@ -44,3 +44,35 @@ end
 # code from tutorial:
 # https://code.likeagirl.io/dont-be-scared-of-api-s-a-beginner-s-guide-to-api-s-using-meetup-and-rails-part-1-7fac68635c49
 # https://code.likeagirl.io/dont-be-scared-of-api-s-4dd9e84271d4
+
+# Dannys code:
+# # PetFinder.rb
+#
+# class Petfinder
+#   require 'httparty'
+#   include HTTParty
+#    # might not want to set base_uri here...not sure if it will set it for just this model or application-wide, or what side effects this has...but you could just include it in your get request
+#   base_uri "api.petfinder.com"
+#
+#   attr_reader :options
+#
+#   def initialize
+#     # do nothing special here
+#   end
+#
+#   # define a _class_ method because it isn't dependent on a specific instance of PetFinder
+#   def self.shelters(location)
+#      api_key = '4e7ed7382fc8429bc103b2e8a8d4e193'
+#      @options = {
+#        query: {
+#          key: api_key,
+#          format: 'json',
+#          count: 10,
+#          location: location
+#       }
+#     }
+#    # should implicitly call self.class.get
+#     get('/shelter.find', @options)
+#   end
+#
+# end
